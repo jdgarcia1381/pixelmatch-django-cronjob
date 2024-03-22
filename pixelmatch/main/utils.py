ac08d1f16d2220558a3c75e5183c224b4da6bc17
Juan David García Manrique <jdgarcia1381@gmail.com>
2024-03-22 18:41:32 -0500

    Add commented pixelmatch func

--- pixelmatch/main/utils.py
+++ pixelmatch/main/utils.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+# from pixelmatch import pixelmatch
 
 
 def get_url_screenshot(url):
@@ -8,4 +9,14 @@ def get_url_screenshot(url):
         page.goto(url, wait_until='networkidle')
         screenshot = page.screenshot()
         browser.close()
+        print(type(screenshot))
         return screenshot
+
+
+# def get_num_diff_pixels(img1, img2):
+#     # TODO: convert byte img to d1 array
+#     # TODO: calculate diff
+#     diff = []
+#     num_diff_pixels = pixelmatch(img1, img2, 1280, 720, diff, threshold=0.1)
+#     print(num_diff_pixels)
+#     return num_diff_pixels
--- pixelmatch/main/views.py
+++ pixelmatch/main/views.py
@@ -7,5 +7,4 @@ from .utils import get_url_screenshot
 def index(request):
     url = "https://play.pixels.xyz/pixels/share/3173"
     screenshot = get_url_screenshot(url)
-    print(screenshot)
     return HttpResponse(screenshot, content_type="image/jpeg")
